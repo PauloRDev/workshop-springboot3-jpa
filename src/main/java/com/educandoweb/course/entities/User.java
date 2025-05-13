2f03a8c6933c753740cd7794d1752aa761d0fbad
Paulo Roberto <pr98.dev@gmail.com>
2025-05-13 10:39:49 -0300

    Order, Instant, ISO 8601 - PT 2

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,6 +21,7 @@ public class User implements Serializable {
 	private String password;
 	private String phone;
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 
